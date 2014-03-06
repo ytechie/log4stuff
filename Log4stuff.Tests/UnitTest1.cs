0c19f8ff99d128bf20737d83dd87d6095dc836ef
Jason Young <jason@ytechie.com>
2014-03-06 16:47:28 -0600

    Added a load test

--- Log4stuff.Tests/UnitTest1.cs
+++ Log4stuff.Tests/UnitTest1.cs
@@ -1,5 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Reflection;
+using System.Text;
+using System.Threading;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
 using Log4stuff.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,7 +22,7 @@ namespace Log4stuff.Tests
         {
             Stream embeddedStream =
                 Assembly.GetExecutingAssembly()
-                    .GetManifestResourceStream(typeof (UnitTest1).Namespace + "." + "SampleLog4jXml.txt");
+                    .GetManifestResourceStream(typeof(UnitTest1).Namespace + "." + "SampleLog4jXml.txt");
 
             string xml = new StreamReader(embeddedStream).ReadToEnd();
             string json = MvcApplication.ConvertLog4JToJson(xml);
@@ -26,12 +35,37 @@ namespace Log4stuff.Tests
         {
             Stream embeddedStream =
                 Assembly.GetExecutingAssembly()
-                    .GetManifestResourceStream(typeof (UnitTest1).Namespace + "." + "SampleLog4jXml.txt");
+                    .GetManifestResourceStream(typeof(UnitTest1).Namespace + "." + "SampleLog4jXml.txt");
 
             string xml = new StreamReader(embeddedStream).ReadToEnd();
             string applicationId = MvcApplication.GetApplicationId(xml);
 
             Assert.AreEqual("sample-app-id", applicationId);
+        }
+
+        [TestMethod]
+        public void MessageFlood()
+        {
+            var udpAppender = new UdpAppender
+            {
+                RemoteAddress = IPAddress.Parse("137.135.86.175"),
+                RemotePort = 8080,
+                Name = "UDPAppender",
+                Encoding = new ASCIIEncoding(),
+                Threshold = Level.Debug,
+                Layout = new XmlLayoutSchemaLog4j()
+            };
+            udpAppender.ActivateOptions();
+
+            BasicConfigurator.Configure(udpAppender);
+
+            var log = LogManager.GetLogger("Simulator");
+            log4net.GlobalContext.Properties["ApplicationId"] = "902245b6-aec4-4057-bd7f-b78264cf9455";
+
+            for (var i = 0; i < 10000; i++)
+            {
+                log.DebugFormat("Flood #{0}", i);
+            }
         }
     }
 }
