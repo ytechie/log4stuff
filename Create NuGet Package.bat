3c2585c20ae6deecc19cb5cdc418c760030d5720
Jason Young <jason@ytechie.com>
2014-03-10 17:07:33 -0500

    New appender and config instructions

---
+++ Create NuGet Package.bat
@@ -0,0 +1,4 @@
+cd Log4stuff.Appender
+nuget pack Log4stuff.Appender.csproj
+move Log4stuff.Appender.*.nupkg ..
+pause
---
+++ Log4stuff.Appender/Log4stuffAppender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using log4net;
+using log4net.Appender;
+using log4net.Layout;
+
+namespace Log4stuff.Appender
+{
+    public class Log4stuffAppender : UdpAppender
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public string ApplicationId { private get; set; }
+
+        public override void ActivateOptions()
+        {
+            var ip = Dns.GetHostEntry("log4stuff.com");
+            RemoteAddress = ip.AddressList[0];
+            RemotePort = 8080;
+            base.Layout = new XmlLayoutSchemaLog4j();
+
+            log4net.GlobalContext.Properties["ApplicationId"] = ApplicationId;
+
+            var logUrl = string.Format("http://log4stuff.com/app/{0}", ApplicationId);
+            Console.WriteLine(string.Format("Now logging to {0}", logUrl));
+            Log.DebugFormat("Now logging to {0}", logUrl);
+
+            base.ActivateOptions();
+        }
+    }
+}
