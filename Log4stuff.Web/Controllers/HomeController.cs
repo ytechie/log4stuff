5fd81f24606b582cf2951cb505f4747537c36b00
Jason Young <jason@ytechie.com>
2014-03-10 16:08:35 -0500

    Updated FAQ + other tweaks

--- Log4stuff.Web/Controllers/HomeController.cs
+++ Log4stuff.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Log4stuff.Web.Controllers
 {
@@ -12,14 +13,21 @@ namespace Log4stuff.Web.Controllers
             return View();
         }
 
+        //[OutputCache(Duration = 600, VaryByParam = "none", Location = OutputCacheLocation.Server)]
         public ActionResult About()
+        {
+            return View();
+        }
+
+       // [OutputCache(Duration = 600, VaryByParam = "none", Location = OutputCacheLocation.Server)]
+        public ActionResult Pricing()
         {
             return View();
         }
 
         public ActionResult GetTheCode(string id)
         {
-            return RedirectToAction("Configure", "Apps", new {id});
+            return RedirectToAction("Configure", "Apps", new { id });
         }
     }
 }
