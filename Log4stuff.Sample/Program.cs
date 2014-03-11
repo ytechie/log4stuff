c997636687e05b432f16dbdc36516daf43cbb7c8
Jason Young <jason@ytechie.com>
2014-03-11 09:41:29 -0500

    Config page revamp and sample app created

---
+++ Log4stuff.Sample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+
+namespace Log4stuff.Sample
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main()
+        {
+            ConfigureWithCode();
+
+            var timer = new Timer((state) => Log.Debug("Sample Message"), null, 1000, 1000);
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+
+        private static void ConfigureWithCode()
+        {
+            var udpAppender = new UdpAppender
+            {
+                RemoteAddress = Dns.GetHostAddresses("log4stuff.com").First(),
+                RemotePort = 8080,
+                Layout = new XmlLayoutSchemaLog4j()
+            };
+            udpAppender.ActivateOptions();
+
+            BasicConfigurator.Configure(udpAppender);
+
+            var applicationId = Guid.NewGuid();
+            log4net.GlobalContext.Properties["ApplicationId"] = applicationId;
+            System.Diagnostics.Process.Start("http://log4stuff.com/app/" + applicationId);
+        }
+    }
+}
